13b6175db46f916ec66a8f80f6fad5be2b0cda51
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-04-29 23:52:11 -0500

    complements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -8,21 +7,20 @@ import { HomeComponent } from './components/home/home.component';
 import { TableOffersComponent } from './components/table-offers/table-offers.component';
 import { NewOfferComponent } from './components/new-offer/new-offer.component';
 import { EditOfferComponent } from './components/edit-offer/edit-offer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ToolbarComponent,
-    HomeComponent,
-    TableOffersComponent,
-    NewOfferComponent,
-    EditOfferComponent
+  AppComponent,
+  ToolbarComponent,
+  HomeComponent,
+  TableOffersComponent,
+  NewOfferComponent,
+  EditOfferComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
